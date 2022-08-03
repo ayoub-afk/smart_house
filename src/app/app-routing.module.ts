ed162ce1944fa2906cfa8a621a015d9d6ef56929
unknown <ayoubmessaadi9@gmail.com>
2022-08-04 01:14:02 +0300

    70%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LogComponent } from './log/log.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SignComponent } from './sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'app-home' ,component: HomeComponent},
+  {path:'app-about', component:AboutComponent},
+  {path:'app-pricing', component:PricingComponent},
+  {path:'app-log', component:LogComponent},
+  {path:'app-sign', component:SignComponent},
+  {path:'app-contact', component:ContactComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
+import { LogComponent } from './log/log.component';
+import { SignComponent } from './sign/sign.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    PricingComponent,
+    ContactComponent,
+    LogComponent,
+    SignComponent
   ],
   imports: [
     BrowserModule,

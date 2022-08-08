d6d49620733f455004a1e9aa145166cf6c66146a
Rim-Ben-Hamouda <110173855+Rim-Ben-Hamouda@users.noreply.github.com>
2022-08-08 10:30:30 +0100

    TEST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,16 @@ import { PricingComponent } from './pricing/pricing.component';
 import { ContactComponent } from './contact/contact.component';
 import { LogComponent } from './log/log.component';
 import { SignComponent } from './sign/sign.component';
+<<<<<<< Updated upstream
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+=======
+import { FooterComponent } from './footer/footer.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -26,7 +30,8 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     PricingComponent,
     ContactComponent,
     LogComponent,
-    SignComponent
+    SignComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
